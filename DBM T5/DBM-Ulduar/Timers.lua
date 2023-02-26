bafa4ef82f6ed85d5320fb9d37698ebbe3ee455a
telkar-rg <telkar-rg@gmx.net>
2023-02-26 13:15:56 +0100

    added timer file for ulduar

---
+++ DBM T5/DBM-Ulduar/Timers.lua
@@ -0,0 +1,11 @@
+local _, TableShared = ...
+
+local Timers = {
+	["FlameLeviathan"] = {
+		["SystemOverload"] = 10,
+		["FlameVents"] = 10,
+		["Pursued"] = 30,
+	},
+}
+
+TableShared.Timers = Timers
