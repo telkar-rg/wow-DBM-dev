f37a9c8c9c244e4d62039b63af0e9e848df20871
telkar-rg <telkar-rg@gmx.net>
2023-07-01 11:55:11 +0200

    Locale Yogg
    Added file containing most/all translation of yogg texts

---
+++ DBM T5/DBM-Ulduar/NPC Translation/locale yogg.lua
@@ -0,0 +1,227 @@
+34342	0	''	'HELP ME! Please get them off of me!'	0	0	0	0	0	0	0	0	1	18019
+34343	0	''	'What do you want from me? Leave me alone!'	0	0	0	0	0	0	0	0	1	18019
+34344	0	''	'Yes! YES! Show them no mercy! Give no pause to your attacks!'	0	0	0	0	0	0	0	0	1	18019
+34345	0	''	'Let hatred and rage guide your blows!'	0	0	0	0	0	0	0	0	1	18019
+34346	0	''	'The time to strike at the head of the beast will soon be upon us! Focus your anger and hatred on his minions!'	0	0	0	0	0	0	0	0	1	18019
+34347	0	''	'Suffocate upon your own hate!'	0	0	0	0	0	0	0	0	1	18019
+34348	0	''	'Tremble, mortals, before the coming of the end!'	0	0	0	0	0	0	0	0	1	18019
+34349	0	''	'Powerless to act....'	0	0	0	0	0	0	0	0	1	18019
+34350	0	''	'Could they have been saved?'	0	0	0	0	0	0	0	0	1	18019
+34351	0	''	'I am the lucid dream.'	0	0	0	0	0	0	0	0	1	18019
+34352	0	''	'The monster in your nightmares.'	0	0	0	0	0	0	0	0	1	18019
+34353	0	''	'The fiend of a thousand faces.'	0	0	0	0	0	0	0	0	1	18019
+34356	0	''	'Cower before my true form.'	0	0	0	0	0	0	0	0	1	18019
+34357	0	'BOW DOWN BEFORE THE GOD OF DEATH!'	''	0	0	0	0	0	0	0	0	1	18019
+34359	0	'Weak-minded fool!'	''	0	0	0	0	0	0	0	0	1	18019
+34360	0	'Look upon the true face of death, and know that your end comes soon!'	''	0	0	0	0	0	0	0	0	1	18019
+34361	0	'MADNESS WILL CONSUME YOU!'	''	0	0	0	0	0	0	0	0	1	18019
+34362	0	'Eternal suffering awaits!'	''	0	0	0	0	0	0	0	0	1	18019
+34363	0	'Your will is no longer your own.'	''	0	0	0	0	0	0	0	0	1	18019
+34364	0	'Destroy them, minion! Your master commands it!'	''	0	0	0	0	0	0	0	0	1	18019
+34365	0	'Your fate is sealed. The end of days is finally upon you and ALL who inhabit this miserable little seedling. Uulwi ifis halahs gag erh\'ongg w\'ssh.'	''	0	0	0	0	0	0	0	0	1	18019
+34366	0	'The veil of winter will protect you, champions!'	''	0	0	0	0	0	0	0	0	1	18019
+34367	0	''	'Eonar, your servant calls for your blessing!'	0	0	0	0	0	0	0	0	1	18019
+34368	0	'Combat matrix enhanced. Behold wonderous rapidity!'	''	0	0	0	0	0	0	0	0	1	18019
+34369	0	'Golganneth, lend me your strength! Grant my mortal allies the power of thunder!'	''	0	0	0	0	0	0	0	0	1	18019
+
+34342	'deDE'	''	'HELFT MIR! Bitte, haltet sie von mir fern!'	18019
+34342	'esES'	''	'¡AYUDADME! ¡Quitádmelos!'	18019
+34342	'esMX'	''	'¡AYÚDENME! ¡Sáquenmelos de encima, por favor!'	18019
+34342	'frFR'	''	'AU SECOURS ! Je vous en prie, arrêtez-les !'	18019
+34342	'koKR'	''	'으아아아아아! 살려주세요! 이것들 좀 떼주세요!'	18019
+34342	'ruRU'	''	'НА ПОМОЩЬ! Помогите отделаться от них!'	18019
+34342	'zhCN'	''	'救命啊！别让它们靠近我！'	18019
+34342	'zhTW'	''	'救命!別讓他們靠近我!'	18019
+34343	'deDE'	''	'Was wollt ihr von mir? Lasst mich in Ruhe!'	18019
+34343	'esES'	''	'¿Qué queréis de mí? Dejadme sola.'	18019
+34343	'esMX'	''	'¿Qué quieres de mí? ¡Déjame en paz!'	18019
+34343	'frFR'	''	'Qu\'est-ce que vous me voulez ? Laissez-moi tranquille !'	18019
+34343	'koKR'	''	'대체 왜 이러는 거지? 저리 가!'	18019
+34343	'ruRU'	''	'Что вам нужно? Прочь от меня!'	18019
+34343	'zhCN'	''	'你们想把我怎么样？别过来！'	18019
+34343	'zhTW'	''	'你們想要做什麼?離我遠一點!'	18019
+34344	'deDE'	''	'Ja! JA! Gewährt ihnen keine Gnade! Lasst sie nicht zu Atem kommen!'	18019
+34344	'esES'	''	'¡Sí! ¡SÍ! ¡No mostréis misericordia! ¡No ceséis vuestros ataques!'	18019
+34344	'esMX'	''	'¡Sí! ¡SÍ! ¡Sin misericordia! ¡No detengas tus ataques ni por un momento!'	18019
+34344	'frFR'	''	'Oui ! OUI ! Pas de pitié pour eux ! Attaquez sans répit !'	18019
+34344	'koKR'	''	'좋아요! 네! 자비를 베풀지 마세요! 쉴 새 없이 공격하세요!'	18019
+34344	'ruRU'	''	'Да! ДА! Не щадите их! Бейте без промаха!'	18019
+34344	'zhCN'	''	'对！没错！不要手下留情！不要停止攻击！'	18019
+34344	'zhTW'	''	'對!沒錯!別對他們手下留情!別停下你的攻擊!'	18019
+34345	'deDE'	''	'Lasst Eure Angriffe vom Hass und vom Zorn leiten!'	18019
+34345	'esES'	''	'¡Dejad que el odio y la ira guíen vuestros golpes!'	18019
+34345	'esMX'	''	'¡Deja que el odio y la ira guíen tus golpes! '	18019
+34345	'frFR'	''	'Laissez la haine et la colère guider vos bras !'	18019
+34345	'koKR'	''	'증오와 분노를 마음껏 터뜨리세요!'	18019
+34345	'ruRU'	''	'Пусть ненависть и ярость направляют ваши удары!'	18019
+34345	'zhCN'	''	'让仇恨和愤怒指引你们战斗吧！'	18019
+34345	'zhTW'	''	'讓仇恨與狂怒帶領你的攻擊!'	18019
+34346	'deDE'	''	'Bald ist die Zeit gekommen, dem Untier den Kopf abzuschlagen! Konzentriert Euren Zorn und Euren Hass auf seine Diener!'	18019
+34346	'esES'	''	'¡Pronto llegará la hora de golpear la cabeza del monstruo! ¡Centrad vuestra ira y odio en sus esbirros!'	18019
+34346	'esMX'	''	'¡El momento de atacar la cabeza de la bestia está por llegar! ¡Enfoca tu furia y tu odio en sus lacayos!'	18019
+34346	'frFR'	''	'Il sera bientôt temps de frapper la tête de la bête ! Concentrez votre rage et votre haine sur ses laquais !'	18019
+34346	'koKR'	''	'짐승의 대장을 칠 때가 곧 다가올 거예요! 놈의 졸개들에게 노여움과 미움을 쏟아부으세요!'	18019
+34346	'ruRU'	''	'Скоро мы сразимся с главарем этих извергов! Обратите гнев и ненависть против его прислужников!'	18019
+34346	'zhCN'	''	'攻击这头野兽要害的时刻即将来临！将你们的愤怒和仇恨倾泻到它的爪牙身上！'	18019
+34346	'zhTW'	''	'我們即將有機會打擊怪物的首腦!現在將你的憤怒與仇恨貫注在他的爪牙上!'	18019
+34347	'deDE'	''	'Erstickt an Eurem eigenen Hass!'	18019
+34347	'esES'	''	'¡Asfíxiate con tu propio odio!'	18019
+34347	'esMX'	''	'¡Sofócate con tu propio odio!'	18019
+34347	'frFR'	''	'Que votre propre haine vous suffoque !'	18019
+34347	'koKR'	''	'네 증오에 숨 막혀 버려라!'	18019
+34347	'ruRU'	''	'Задохнитесь в собственной ненависти!'	18019
+34347	'zhCN'	''	'被你们自己的仇恨吞噬吧！'	18019
+34347	'zhTW'	''	'在你自己的仇恨中窒息吧!'	18019
+34348	'deDE'	''	'Erzittert, Sterbliche, denn das sichere Ende ist nah!'	18019
+34348	'esES'	''	'¡Temblad, mortales, ante la llegada del fin!'	18019
+34348	'esMX'	''	'¡Tiemblen, mortales, ante la llegada del fin!'	18019
+34348	'frFR'	''	'Tremblez, mortels, avant que votre fin n\'arrive !'	18019
+34348	'koKR'	''	'다가오는 종말 앞에 몸서리쳐라!'	18019
+34348	'ruRU'	''	'Трепещите, смертные, конец близок!'	18019
+34348	'zhCN'	''	'战栗吧，凡人们，末日就要来临了！'	18019
+34348	'zhTW'	''	'在即將到來的末日之前，顫抖吧，凡人。'	18019
+34349	'deDE'	''	'Zu schwach, um zu kämpfen...'	18019
+34349	'esES'	''	'No puedes actuar…'	18019
+34349	'esMX'	''	'Sin poder para actuar…'	18019
+34349	'frFR'	''	'Incapable !'	18019
+34349	'koKR'	''	'무력하기는...'	18019
+34349	'ruRU'	''	'Какое бессилие...'	18019
+34349	'zhCN'	''	'毫无力量……'	18019
+34349	'zhTW'	''	'沒用的廢物...'	18019
+34350	'deDE'	''	'Hätten sie gerettet werden können?'	18019
+34350	'esES'	''	'¿Se podrían haber salvado?'	18019
+34350	'esMX'	''	'¿Podrían haber sido salvados?'	18019
+34350	'frFR'	''	'Auraient-ils pu être sauvés ?'	18019
+34350	'koKR'	''	'살릴 수도 있었나?'	18019
+34350	'ruRU'	''	'Был ли у них шанс на спасение?'	18019
+34350	'zhCN'	''	'他们没救了吗？'	18019
+34350	'zhTW'	''	'他們能夠獲得拯救嗎?'	18019
+34351	'deDE'	''	'Ich bin der strahlende Traum.'	18019
+34351	'esES'	''	'Soy un sueño lúcido.'	18019
+34351	'esMX'	''	'Soy el sueño lúcido.'	18019
+34351	'frFR'	''	'Je suis le rêve éveillé.'	18019
+34351	'koKR'	''	'나는, 살아 있는 꿈이다.'	18019
+34351	'ruRU'	''	'Я – это сон наяву.'	18019
+34351	'zhCN'	''	'我是清醒的梦境。'	18019
+34351	'zhTW'	''	'我是清醒的夢境。'	18019
+34352	'deDE'	''	'Das Monster in Euren Alpträumen!'	18019
+34352	'esES'	''	'El monstruo de vuestras pesadillas.'	18019
+34352	'esMX'	''	'El monstruo de tus pesadillas.'	18019
+34352	'frFR'	''	'Le monstre de vos cauchemars.'	18019
+34352	'koKR'	''	'악몽 속의 괴물이다.'	18019
+34352	'ruRU'	''	'Чудовище из ваших кошмаров.'	18019
+34352	'zhCN'	''	'你们噩梦中的怪物。'	18019
+34352	'zhTW'	''	'你噩夢中的怪物。'	18019
+34353	'deDE'	''	'Der Verderber mit den tausend Gesichtern.'	18019
+34353	'esES'	''	'El demonio de mil caras.'	18019
+34353	'esMX'	''	'El demonio de mil caras.'	18019
+34353	'frFR'	''	'Le démon aux mille visages.'	18019
+34353	'koKR'	''	'수천의 얼굴을 지닌 마귀다.'	18019
+34353	'ruRU'	''	'Демон с тысячью лиц.'	18019
+34353	'zhCN'	''	'千面的恶魔。'	18019
+34353	'zhTW'	''	'有著千般面貌的惡魔。'	18019
+----
+34356	'deDE'	''	'Kriecht vor meiner wahren Natur!'	18019
+----
+34356	'esES'	''	'Temed mi verdadera forma.'	18019
+----
+34356	'esMX'	''	'Tiembla ante mi verdadera forma.'	18019
+----
+34356	'frFR'	''	'Tremblez devant mon véritable aspect.'	18019
+----
+34356	'koKR'	''	'진정한 나를 보고 몸서리쳐라.'	18019
+----
+34356	'ruRU'	''	'Трепещите, ибо я обретаю истинную форму.'	18019
+----
+34356	'zhCN'	''	'在我的真正形态面前颤抖吧。'	18019
+----
+34356	'zhTW'	''	'在我的真身面前顫抖吧。'	18019
+----
+34359	'deDE'	'Schwachsinniger Narr!'	''	18019
+34359	'esES'	'¡Loco descerebrado!'	''	18019
+34359	'esMX'	'¡Tonto, débil mental!'	''	18019
+34359	'frFR'	'Imbécile faible d\'esprit !'	''	18019
+34359	'koKR'	'의지가 박약한 자여!'	''	18019
+34359	'ruRU'	'Вот тупица!'	''	18019
+34359	'zhCN'	'意志薄弱的蠢货！'	''	18019
+34359	'zhTW'	'心智脆弱的傻瓜!'	''	18019
+34360	'deDE'	'Erblickt das wahre Antlitz des Todes und wisset: Euer Ende ist nah!'	''	18019
+34360	'esES'	'¡Observad el auténtico rostro de la muerte y descubrid que vuestro fin está a punto de llegar!'	''	18019
+34360	'esMX'	'¡Mira el verdadero rostro de la muerte y sabrás que tú fin se acerca!'	''	18019
+34360	'frFR'	'Contemplez le vrai visage de la mort, et sachez que votre fin approche !'	''	18019
+34360	'koKR'	'죽음의 진정한 얼굴을 보아라! 다가오는 종말을 느껴라!'	''	18019
+34360	'ruRU'	'Взгляните в истинное лицо смерти и знайте, что ваш конец близок!'	''	18019
+34360	'zhCN'	'凝视死亡的真正面孔吧，你们的末日就要来了！'	''	18019
+34360	'zhTW'	'看看死亡的真實面貌，瞭解你們的末日降臨了!'	''	18019
+34361	'deDE'	'DER WAHNSINN WIRD EUCH VERSCHLINGEN!'	''	18019
+34361	'esES'	'¡LA LOCURA TE CONSUMIRÁ!'	''	18019
+34361	'esMX'	'¡LA LOCURA TE CONSUMIRÁ!'	''	18019
+34361	'frFR'	'LA FOLIE VOUS EMPORTERA !'	''	18019
+34361	'koKR'	'광기가 너희를 삼키리라!'	''	18019
+34361	'ruRU'	'БЕЗУМИЕ ПОГЛОТИТ ВАС!'	''	18019
+34361	'zhCN'	'疯狂将把你彻底吞噬！'	''	18019
+34361	'zhTW'	'瘋狂會將你們吞噬殆盡!'	''	18019
+34362	'deDE'	'Euch erwartet Ewiges Leid!'	''	18019
+34362	'esES'	'¡El sufrimiento eterno aguarda!'	''	18019
+34362	'esMX'	'¡Te espera el sufrimiento eterno!'	''	18019
+34362	'frFR'	'Une souffrance éternelle vous attend !'	''	18019
+34362	'koKR'	'영원한 고통이 기다린다!'	''	18019
+34362	'ruRU'	'Впереди вечные страдания!'	''	18019
+34362	'zhCN'	'永恒的痛苦在等待着你！'	''	18019
+34362	'zhTW'	'永恆的苦難等著你!'	''	18019
+34363	'deDE'	'Euer Wille gehorcht Euch nicht länger.'	''	18019
+34363	'esES'	'Tu voluntad ya no te pertenece.'	''	18019
+34363	'esMX'	'Tu voluntad ya no es tuya.'	''	18019
+34363	'frFR'	'Ta volonté ne t\'appartient plus.'	''	18019
+34363	'koKR'	'너의 의지가 너를 배반한다.'	''	18019
+34363	'ruRU'	'Ты теперь в моей власти.'	''	18019
+34363	'zhCN'	'你的意志不属于你自己。'	''	18019
+34363	'zhTW'	'你的意志不再屬於你自己。'	''	18019
+34364	'deDE'	'Vernichte Sie, mein Diener! Dein Meister befiehlt es dir!'	''	18019
+34364	'esES'	'¡Destrúyelos, esbirro! ¡Tu maestro te lo ordena!'	''	18019
+34364	'esMX'	'¡Destrúyelo, lacayo! ¡Te lo ordena tu amo!'	''	18019
+34364	'frFR'	'Détruis-les, laquais ! Ton maître te l\'ordonne !'	''	18019
+34364	'koKR'	'저들을 없애라! 네 주인이 명하노라!'	''	18019
+34364	'ruRU'	'Слуга, убей их! Исполни приказ хозяина!'	''	18019
+34364	'zhCN'	'我的奴仆，快去毁灭他们！这是主人的命令！'	''	18019
+34364	'zhTW'	'摧毀他們，手下們!這是你主人的命令!'	''	18019
+34365	'deDE'	'Euer Schicksal ist besiegelt. Das Ende aller Tage steht Euch bevor, so wie ALLEN, die diesen armseligen Planeten bewohnen. Uulwi ifis halahs gag erh\'ongg w\'ssh.'	''	18019
+34365	'esES'	'Vuestra suerte está echada. Por fin se acerca el final de vuestros días y el de todos los que viven en este miserable semillero. Uulwi ifis halahs gag erh\'ongg w\'ssh.'	''	18019
+34365	'esMX'	'Tu destino fue sellado. El fin de los tiempos ha llegado para ti y para TODOS los habitantes de este retoñito miserable. Uulwi ifis halahs gag erh\'ongg w\'ssh.'	''	18019
+34365	'frFR'	'Votre destin est scellé. La fin des temps est enfin arrivée pour vous et tous les habitants de ce misérable petit bourgeon. Uulwi ifis halahs gag erh\'ongg w\'ssh.'	''	18019
+34365	'koKR'	'너희 운명이 결정됐다. 너희와, 이 작고 가련한 세상에 사는 모든 것들에게 마침내 종말이 닥친다. 우울위 이피스 할란 가그 에르옹그 위스...'	''	18019
+34365	'ruRU'	'Вы обречены. Ни вы, никто другой НЕ В СИЛАХ избежать близящегося конца света. Уульви ифис халас гаг эрх\'онг ушшш!'	''	18019
+34365	'zhCN'	'你们的命运早已注定。末日即将降临于这个可笑而渺小的世界，你们所有人都要灭亡。Uulwi ifis halahs gag erh\'ongg w\'ssh.'	''	18019
+34365	'zhTW'	'你們的命運已經注定。末日到來，所有居住在這可悲幼苗上的生物將無一倖免。(古神語)寄宿在我屍體上的陰影將永世侵擾這片大地。'	''	18019
+34366	'deDE'	'Der Schleier des Winter wird Euch beschützen, Helden!'	''	18019
+34366	'esES'	'¡El velo del invierno os protegerá, campeones!'	''	18019
+34366	'esMX'	'¡El velo del invierno los protegerá, campeones!'	''	18019
+34366	'frFR'	'Le voile de l\'hiver vous protègera, champions !'	''	18019
+34366	'koKR'	'용사들이여, 겨울의 장막이 너희를 보호하리라!'	''	18019
+34366	'ruRU'	'Герои, вас защитит покров зимы!'	''	18019
+34366	'zhCN'	'寒冬将会庇护你们，勇士们！'	''	18019
+34366	'zhTW'	'凜冬之幕將守護著你，勇士們!'	''	18019
+34367	'deDE'	''	'Eonar, Eure Dienerin bittet um Euren Segen!'	18019
+34367	'esES'	''	'¡Eonar, tus sirvientes piden tu bendición!'	18019
+34367	'esMX'	''	'¡Eonar, tu siervo necesita de tu bendición!'	18019
+34367	'frFR'	''	'Eonar, ta servante implore ta bénédiction !'	18019
+34367	'koKR'	''	'이오나여, 당신의 종이 축복을 기원합니다!'	18019
+34367	'ruRU'	''	'Эонар, прошу тебя, даруй свое благословение!'	18019
+34367	'zhCN'	''	'艾欧娜尔，请祝福您的仆人！'	18019
+34367	'zhTW'	''	'伊歐娜，你的僕從請求你的祝福!'	18019
+34368	'deDE'	'Kampfmatrix verbessert. Werdet Zeugen außergewöhnlicher Geschwindigkeit!'	''	18019
+34368	'esES'	'Matriz de energía mejorada. ¡Observad su maravillosa rapidez!'	''	18019
+34368	'esMX'	'Matriz de combate mejorada. ¡Observen la extraordinaria rapidez!'	''	18019
+34368	'frFR'	'Matrice de combat améliorée. Contemplez tant de merveilleuse rapidité !'	''	18019
+34368	'koKR'	'전투 회로망 강화! 놀라운 속도를 즐기라고!'	''	18019
+34368	'ruRU'	'Боевая матрица расширена. Какая невероятная скорость!'	''	18019
+34368	'zhCN'	'战斗矩阵已强化。全速突击！'	''	18019
+34368	'zhTW'	'戰鬥矩陣已經強化。瞧瞧這驚人的速度!'	''	18019
+34369	'deDE'	'Golganneth, leiht mir Eure Stärke! Gewährt meinen sterblichen Verbündeten die Macht des Donners!'	''	18019
+34369	'esES'	'¡Golganneth, préstame tu fuerza! ¡Otorga a mis aliados mortales el poder del trueno!'	''	18019
+34369	'esMX'	'¡Golganneth, dame tu fuerza! ¡Brinda a mis aliados mortales el poder del trueno!'	''	18019
+34369	'frFR'	'Golganneth, prête-moi ta force ! Accorde à mes alliés mortels la puissance du tonnerre !'	''	18019
+34369	'koKR'	'골가네스여, 제게 힘을 주소서! 필멸의 동료에게 번개의 힘을 나눠주소서!'	''	18019
+34369	'ruRU'	'Голганнет, даруй мне силу! Надели моих смертных союзников мощью грома!'	''	18019
+34369	'zhCN'	'高戈奈斯，将您的威能赐予我！让我的战友们获得雷霆的力量！'	''	18019
+34369	'zhTW'	'葛剛尼斯，將你的神力賜給我!賦予我的凡人盟友們雷霆的力量!'	''	18019
